6acbd356ca982ca9c98cd5d869c104ade1dff8a7
henosBot <spi0003@boxhillhs.vic.edu.au>
2021-02-12 23:22:19 +1100

    import cogs

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from tools.mlmd_bot import MLMD_bot
 from discord_slash import SlashCommand
 import dotenv
 import os
+import jishaku
 
 dotenv.load_dotenv()
 token = os.getenv('TOKEN')
@@ -11,6 +12,16 @@ bot = MLMD_bot()
 bot.slash = SlashCommand(bot, auto_register=True)
 bot.set_embed_color(discord.Colour.red())
 
-bot.load_extension('cogs.submissions')
+bot.remove_command('help')
+extentions = [
+  'cogs.henostools',
+  'cogs.other',
+  'cogs.submissions',
+  'cogs.dmgcalc',
+  'jishaku',
+  'help'
+]
+for extention in extentions:
+    bot.load_extension(extention)
 
 bot.run(token)
